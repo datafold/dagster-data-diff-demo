983b1e5698faa5102435c416c450b279c65dffa1
Sung Won Chung <sung@datafold.com>
2023-10-23 11:45:51 -0700

    more setup

--- data-diff-demo/data_diff_demo/__init__.py
+++ data-diff-demo/data_diff_demo/__init__.py
@@ -1,8 +1,8 @@
 from dagster import Definitions, load_assets_from_modules
 
-from . import assets
+from . import assets, simple_demo
 
-all_assets = load_assets_from_modules([assets])
+all_assets = load_assets_from_modules([assets, simple_demo])
 
 defs = Definitions(
     assets=all_assets,
--- data-diff-demo/setup.py
+++ data-diff-demo/setup.py
@@ -4,8 +4,10 @@ setup(
     name="data_diff_demo",
     packages=find_packages(exclude=["data_diff_demo_tests"]),
     install_requires=[
-        "dagster",
-        "dagster-cloud"
+        "dagster==1.5.4",
+        "dagster-cloud==1.5.4",
+        "data-diff==0.9.7",
+        "duckdb==0.9.1"
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
